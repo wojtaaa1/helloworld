8e8ce6c83d936074e265293e2dd8480dc955ba20
Vojtěch Klimenta <klimenta.v@gmail.com>
2024-05-02 21:45:40 +0200

    První lekce Javy Učíme se proměnné a datové typy

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /**
+     * Komentář k metodě main.
+     * @param args
+     */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printingnames();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        printNumbers1To5();
+
+        triangle();
+
+    }
+
+    private static void triangle() {
+        double a = 3.5;
+        double b = 4.5;
+        System.out.println("Úhlopříčka trojúhelníku je: "+calculateDiagonal(a, b));
+
+        double c;
+        c = calculateDiagonal(a = 5, b = 6);
+        System.out.println("Úhlopříčka trojúhelníku je: "+c);
+    }
+
+    private static void printingnames() {
+        System.out.println("Hello and welcome!");
+        System.out.println("I wrote my first code in Java"); // sout
+        System.out.println("I wrote my first code in Java"); // Command - D
+
+        String name = "Karel Dvořák";
+        LocalDate date = LocalDate.now(); //musím naimportovat
+        date = LocalDate.of(2022, 12, 1);
+        System.out.println(date);
+
+        
+    }
+
+    private static void printNumbers1To5() {
+        for (int i = 1; i <= 5; i++) { // zopakuj 5x kód v těle cyklu
             System.out.println("i = " + i);
         }
+    }
+    private static double calculateDiagonal (double a, double b) {
+        return Math.sqrt(a * a + b * b); // výpočet délky úhlopříčky obdélníku
+
+
     }
 }
